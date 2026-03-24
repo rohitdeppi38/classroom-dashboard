65867c6c1e719008cf8e3d1b8dd6d13f554631f2
rohit <rohitdeppi38@gmail.com>
2026-03-24 09:53:37 +0530

    feature/adding classes

--- classroom-frontend/src/App.tsx
+++ classroom-frontend/src/App.tsx
@@ -15,8 +15,14 @@ import { dataProvider } from "./providers/data";
 
 import Dashboard from "./pages/dashboard";
 import { Layout } from "./components/refine-ui/layout/layout";
-import { Book, Home } from "lucide-react";
+import { Book, BookOpen, GraduationCap, Home } from "lucide-react";
+
 import SubjectsLists from './pages/subjects/subjectsLists'
+import SubjectsCreate from './pages/subjects/create'
+
+import ClassesList from './pages/classes/list'
+import ClassesCreate from './pages/classes/create'
+
 
 function App() {
   return (
@@ -36,7 +42,8 @@ function App() {
               resources={
                 [
                   {name:'dashboard',list:'/',meta:{label:'Home',icon:<Home/>}},
-                  {name:'subjects',list:'/subjects',create:'/subjects/create',meta:{label:'Subjects',icon:<Book/>}}
+                  {name:'subjects',list:'/subjects',create:'/subjects/create',meta:{label:'Subjects',icon:<Book/>}},
+                  {name:'classes',list:'/classes',create:'/classes/create',meta:{label:'classes',icon:<GraduationCap/>}}
                 ]
               }
             >
@@ -49,7 +56,11 @@ function App() {
                   <Route path="/" element={<Dashboard/>} />
                   <Route path="subjects">
                     <Route index element={<SubjectsLists/>}></Route>
-                    <Route path="create" element={<SubjectsLists/>}/>
+                    <Route path="create" element={<SubjectsCreate/>}/>
+                  </Route>
+                  <Route path="classes">
+                    <Route index element={<ClassesList/>}/>
+                    <Route path="create" element={<ClassesCreate/>}/>
                   </Route>
                 </Route>
               </Routes>
