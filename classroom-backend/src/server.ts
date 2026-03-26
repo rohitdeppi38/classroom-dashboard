e9be39bbcacd339f6d8b131edef34fddd799d17c
rohit <rohitdeppi38@gmail.com>
2026-03-26 15:58:06 +0530

    feat/security by arcjet

--- classroom-backend/src/server.ts
+++ classroom-backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 
 import subjectsRouter from './routes/subjects';
+import { securityMiddleware } from './middleware/securityMiddleware';
 
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(cors({
 }))
 
 app.use(express.json());
+app.use(securityMiddleware)
 
 app.use('/api/subjects',subjectsRouter);
 
