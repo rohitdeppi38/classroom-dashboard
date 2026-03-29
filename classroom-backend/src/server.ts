d804f42a92ef968c10c11e46f0953522614e6dcf
rohit <rohitdeppi38@gmail.com>
2026-03-29 19:11:40 +0530

    .js extension

--- classroom-backend/src/server.ts
+++ classroom-backend/src/server.ts
@@ -4,12 +4,12 @@ AgentAPI.config();
 import express from 'express'
 import cors from 'cors'
 
-import subjectsRouter from './routes/subjects';
-import { securityMiddleware } from './middleware/securityMiddleware';
+import subjectsRouter from './routes/subjects.js';
+import { securityMiddleware } from './middleware/securityMiddleware.js';
 
 
 import { toNodeHandler } from "better-auth/node";
-import { auth } from './lib/auth';
+import { auth } from './lib/auth.js';
 
 
 const app = express();
