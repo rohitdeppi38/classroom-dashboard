b28a3517148774abeaae65a07ba74b3192ce8eb7
rohit <rohitdeppi38@gmail.com>
2026-04-03 21:40:29 +0530

    adding the deleted post route

--- classroom-backend/src/routes/classes.ts
+++ classroom-backend/src/routes/classes.ts
@@ -80,4 +80,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/',async(req,res)=>{
+    try {
+        const {name,teacherId,subjectId,capacity,description,status,bannerUrl,bannerCldPubId} = req.body;
+        const [createdClass] = await db
+        .insert(classes)
+        .values({...req.body,inviteCode:Math.random().toString(36).substring(2,9),schedule:[]})
+        .returning({id:classes.id})
+
+        if(!createdClass) throw Error;
+         res.status(201).json({data:createdClass});
+    } catch (error) {
+        console.log(`Error in POST / ${error}`);
+        res.status(500).json({error:error});
+    }
+})
+
 export default router;
